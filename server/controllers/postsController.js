34a17c438543d30b0c552d1f2fe6433e231e9015
AyomideKayode <ayomidekay7@gmail.com>
2025-08-05 13:40:38 +0100

    feat: implement update post functionality - added updatePost controller - integrated ObjectId validation - updated posts routes to include PATCH method for post updates.🚧

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -2,6 +2,7 @@
 
 // import schema
 import PostMessage from '../models/postMessage.js';
+import mongoose from 'mongoose';
 
 export const getPosts = async (req, res) => {
   try {
@@ -26,4 +27,22 @@ export const createPost = async (req, res) => {
   } catch (error) {
     res.status(409).json({ message: error.message }); // send error message if something goes wrong
   }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send('No post with that id');
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    {
+      new: true, // return the updated post
+    }
+  );
+
+  res.json(updatedPost); // send the updated post as a JSON response
 };
--- server/routes/postsRoutes.js
+++ server/routes/postsRoutes.js
@@ -1,11 +1,12 @@
 // 'use strict';
 import express from 'express';
 
-import { getPosts, createPost } from '../controllers/postsController.js';
+import { getPosts, createPost, updatePost } from '../controllers/postsController.js';
 
 const router = express.Router();
 
 router.get('/', getPosts); // GET request to /posts will call getPosts controller
 router.post('/', createPost);
+router.patch('/:id', updatePost);
 
 export default router;
